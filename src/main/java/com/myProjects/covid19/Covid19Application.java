d555399cf6a1718572cc8f6676696bf909297d6b
BOUZAABIAM <mohamedyoussefbouzaabia@gmail.com>
2020-04-02 23:22:15 +0200

    create model for Location Stats && run fetch data service every day

--- src/main/java/com/myProjects/covid19/Covid19Application.java
+++ src/main/java/com/myProjects/covid19/Covid19Application.java
@@ -2,8 +2,10 @@ package com.myProjects.covid19;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Covid19Application {
 
 	public static void main(String[] args) {
